c7ff9a130c47e686de9b8137054a0f272d90a1de
jlaron230 <jlaron230@hotmail.fr>
2024-06-04 21:03:18 +0200

    15eme version

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'; // Importer createRoot au lieu de ReactDOM.render
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// Sélectionner le conteneur où l'application sera rendue
-const container = document.getElementById('root');
-
-// Créer une racine avec la nouvelle API createRoot
-const root = createRoot(container);
-
-// Utiliser root.render pour rendre l'application
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
